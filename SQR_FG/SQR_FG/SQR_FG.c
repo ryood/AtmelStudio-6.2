62e559db9a4ba701a3e167906de26c4ad1bd2ff0
ryood <kawanabe@gmail.com>
2015-05-07 19:19:51 +0900

    Add SQR_FG Project

---
+++ SQR_FG/SQR_FG/SQR_FG.c
@@ -0,0 +1,17 @@
+/*
+ * SQR_FG.c
+ *
+ * Created: 2015/05/07 15:41:32
+ *  Author: gizmo
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
