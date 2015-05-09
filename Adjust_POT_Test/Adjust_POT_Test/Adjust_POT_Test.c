fa2b85e3258554043128f8c0f0b0082d6a03cfd1
ryood <kawanabe@gmail.com>
2015-05-09 21:24:11 +0900

    Adjust_POT_Test Update

--- Adjust_POT_Test/Adjust_POT_Test/Adjust_POT_Test.c
+++ Adjust_POT_Test/Adjust_POT_Test/Adjust_POT_Test.c
@@ -9,26 +9,10 @@
 
 //	ATmega328P
 
-/*
-ポート対応	[ATmega328P]	x:存在しない(PC6はRESET) -:空き
-bit		7	6	5	4	3	2	1	0
-portB	-	-	-	-	-	-	-	CKO		PB0:システムクロック出力
-portC	x	x	SCL	SDA	-	-	Fm	FM		I2C(PC5:クロック PC4:データ) ADC(PC1:周波数adj PC0:周波数)
-portD	-	-	-	-	-	RST	-	-		LCD
-
-【ヒューズビット】 16MHz外部クリスタル
-Low: B7 High: D9 Ext: 07
-
-ビルド環境
-AtmelStudio 6.2		最適化オプション:-O1	数学ライブラリを追加:libm.a
-ATmega328P	16MHz（外部クリスタル）
-*/ 
-
-
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
 
-#define sbi(BYTE,BIT) (BYTE|=_BV(BIT)) // BYTEの指定BITに1をセット
+#define sbi(BYTE,BIT) (BYTE|=_BV(BIT))  // BYTEの指定BITに1をセット
 #define cbi(BYTE,BIT) (BYTE&=~_BV(BIT)) // BYTEの指定BITをクリア
 
 static void wait_us(short t)
@@ -62,20 +46,6 @@ void adc_init(void)
 }
 
 /*==============================================
- * 波形生成割込み
- *==============================================*/
-
-//******************************************************************
-// timer0 setup
-// set prescaler to 1, 16,000,000Hz / 16 clock
-//
-
-//******************************************************************
-// Timer2 Interrupt Service at 16,000,000Hz = 1uSec
-//
-ISR(TIMER0_OVF_vect)
-
-/*==============================================
  * 引数：変換するADCチャンネル
  * 返値：ADCの読み取り値(0..1023)
  *==============================================*/
@@ -95,19 +65,16 @@ int16_t adc_convert(int8_t channel)
 int main(void)
 {
 	int interval = 10;
-	int adjust = 10;
-	
+		
 	DDRD = 0x01;
 	
 	adc_init();
 	
     while(1)
     {
-		//interval = adc_convert(0);
-		//adjust = adc_convert(1);
+		interval = adc_convert(0);
 		
 		PORTD ^= 0x01;
-		//wait_ms(interval/10);
-		wait_us(adjust);
+		wait_ms(interval);
     }
 }
